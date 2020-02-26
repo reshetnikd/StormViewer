9fe95ee6a3455eb21c29bcf5c8c04bcb0ed7467f
reshetnikd <dmitry.reshetnik@icloud.com>
2020-02-26 20:46:13 +0200

    Add share UIActivityViewController

--- StormViewer/DetailViewController.swift
+++ StormViewer/DetailViewController.swift
@@ -20,6 +20,7 @@ class DetailViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         title = "Picture \(selectedPictureNumber) of \(totalPictures)"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
         navigationItem.largeTitleDisplayMode = .never
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
@@ -34,6 +35,17 @@ class DetailViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image, selectedImage!], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
 
     /*
