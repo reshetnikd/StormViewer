277ec6b0c0738e3f50732311d4ec5186618049e2
reshetnikd <dmitry.reshetnik@icloud.com>
2020-04-14 22:34:56 +0300

    Replace UITableView with UICollectionView

---
+++ StormViewer/PictureCell.swift
@@ -0,0 +1,16 @@
+//
+//  PictureCell.swift
+//  StormViewer
+//
+//  Created by Dmitry Reshetnik on 14.04.2020.
+//  Copyright © 2020 Dmitry Reshetnik. All rights reserved.
+//
+
+import UIKit
+
+class PictureCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
--- StormViewer/ViewController.swift
+++ StormViewer/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class ViewController: UICollectionViewController {
     var pictures = [String]()
 
     override func viewDidLoad() {
@@ -29,22 +29,49 @@ class ViewController: UITableViewController {
             self.pictures.sort()
             
             DispatchQueue.main.async {
-                self.tableView.reloadData()
+//                self.tableView.reloadData()
+                self.collectionView.reloadData()
             }
         }
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return pictures.count
+//    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return pictures.count
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
-        cell.textLabel?.text = pictures[indexPath.row]
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+//        cell.textLabel?.text = pictures[indexPath.row]
+//        return cell
+//    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Picture", for: indexPath) as? PictureCell else {
+            fatalError()
+        }
+        cell.imageView.image = UIImage(named: pictures[indexPath.item])
+        cell.name.text = pictures[indexPath.item]
+        
         return cell
     }
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        // 1: try loading the "Detail" view controller and typecasting it to be DetailViewController
+//        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+//            // 2: success! Set its selectedImage property
+//            vc.selectedImage = pictures[indexPath.row]
+//            vc.selectedPictureNumber = indexPath.row + 1
+//            vc.totalPictures = pictures.count
+//            // 3: now push it onto the navigation controller
+//            navigationController?.pushViewController(vc, animated: true)
+//        }
+//    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         // 1: try loading the "Detail" view controller and typecasting it to be DetailViewController
         if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
             // 2: success! Set its selectedImage property
